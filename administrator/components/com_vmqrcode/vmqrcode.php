2cf1d4a34c7d5697c36221a7b85ed597d01d7a6e
Hung Tran <nguyentieuhau@gmail.com>
2022-04-14 12:47:38 +0700

    Check VirtueMart availability

--- administrator/components/com_vmqrcode/vmqrcode.php
+++ administrator/components/com_vmqrcode/vmqrcode.php
@@ -11,8 +11,14 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Component\ComponentHelper;
 
 require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/vmqrcode.php';
+
+if (!ComponentHelper::isEnabled('com_virtuemart', true) )
+{
+	return VMQRCodeHelper::throwException(Text::_('COM_VMQRCODE_ERROR_VIRTUEMART_REQUIRED'), 500);
+}
 
 if (!Factory::getUser()->authorise('core.manage', 'com_vmqrcode'))
 {
